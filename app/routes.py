fa2ac71ce89ae35cf55dc50c960aeb755b1b2628
sveta-afanaseva <sveta_afanaseva_1997@mail.ru>
2022-01-10 00:02:00 +0300

    add doctor_view, schedule_view

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,22 @@
+import re
 from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
 import requests
 
 
-@app.route("/specialities", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
+def get_oms():
+    return render_template("index.html")
+
+
+@app.route("/specialities", methods=["POST"])
 def get_specialities():
+    oms_number = request.json.get("oms_number")
+    birth_date = request.json.get("birth_date") 
+    if not oms_number or not birth_date:
+        return {"error": "Не передан номер ОМС или дата рождения"}
     r = requests.post(
         "https://emias.info/api/new/eip5orch?getSpecialitiesInfo", 
         json={
@@ -13,21 +24,100 @@ def get_specialities():
             "params":{"omsNumber": oms_number,"birthDate": birth_date}
         } 
     )
+    if "error" in r.json():
+        return r.json()["error"]["data"]["code"]
     results = r.json()['result']
-    specialities = []
+    specialities = [result['name'] for result in results]
+
+    return str(specialities)    
+
+
+@app.route("/doctors", methods=["POST"])
+def get_doctors():
+    oms_number = request.json.get("oms_number")
+    birth_date = request.json.get("birth_date") 
+    speciality_id = request.json.get("speciality_id")
+    if not oms_number or not birth_date:
+        return {"error": "Не передан номер ОМС или дата рождения"}
+    r = requests.post(
+        "https://emias.info/api/new/eip5orch?getDoctorsInfo", 
+        json={
+            "jsonrpc":"2.0","id":"","method":"getDoctorsInfo",
+            "params":{"omsNumber": oms_number,"birthDate": birth_date, "specialityId": speciality_id}
+        } 
+    )
+    if "error" in r.json():
+        return r.json()["error"]["data"]["code"]
+    results = r.json()['result']
+    doctors = [result['name'] for result in results]
+
+    return str(doctors)
+
+
+@app.route("/shedule", methods=["POST"])
+def get_schedule():
+    hospital_id = request.json.get("hospital_id")
+    available_resource_id = request.json.get("available_resource_id")
+
+    r = requests.post(
+        "https://emias.info/api/new/eip",
+        json={
+            "jsonrpc": "2.0", "id": 1, "method": "get_lpu_schedule_info", 
+            "params": {"lpu_id": hospital_id}
+        }
+    )
+    results = r.json()['result']['availableResource']
+
+    schedule = []
     for result in results:
-        specialities.append(result['name'])
+        if available_resource_id == result['id']:
+            for row in result['schedule']:
+                if 'receptionInfo' in row:
+                    schedule.append({'date': row['date'], 'time_interval': row['receptionInfo']})
 
-    return str(specialities)
-    # return render_template("index.html", title="Home", user=user, posts=posts)
+    return str(schedule)
 
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash(
-            f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}"
-        )
-        return redirect(url_for("index"))
-    return render_template("login.html", title="Sign In", form=form)
+# def get_available_schedule():
+#     oms_number = request.json.get("oms_number")
+#     birth_date = request.json.get("birth_date") 
+#     available_resource_id = request.json.get("available_resource_id")
+#     complex_resource_id = request.json.get("complex_resource_id")
+
+#     if not oms_number or not birth_date:
+#         return {"error": "Не передан номер ОМС или дата рождения"}
+
+#     r = requests.post(
+#         "https://emias.info/api/new/eip5orch?getAvailableResourceScheduleInfo", 
+#         json={
+#             "jsonrpc":"2.0","id":"","method":"getAvailableResourceScheduleInfo",
+#             "params":{"omsNumber": oms_number,"birthDate": birth_date, 
+#             "availableResourceId": available_resource_id, "complexResourceId": complex_resource_id}
+#         } 
+#     )
+#     if "error" in r.json():
+#         return r.json()["error"]["data"]["code"]
+#     results = r.json()['result']['scheduleOfDay']
+#     shedule = []
+#     for result in results:
+#         date = result['date']
+#         shedule.append({'date': date})
+#         for slot in result['scheduleBySlot']['slot']:
+#             start_time = slot['startTime']
+#             if shedule[0].get('start_time') is None:
+#                 pass
+
+#     return str(shedule)
+
+
+# @app.route("/login", methods=["GET", "POST"])
+# def login():
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         flash(
+#             f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}"
+#         )
+#         return redirect(url_for("index"))
+#     return render_template("login.html", title="Sign In", form=form)
+
+
--- project_doctor.py
+++ project_doctor.py
@@ -5,3 +5,6 @@ from app.models import User, Appointment
 @app.shell_context_processor
 def make_shell_context():
     return {"db": db, "User": User, "Appointment": Appointment}
+
+if __name__ == "__main__":
+    app.run(debug=True)
